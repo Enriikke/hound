915ea1913f00b96a4ac91c46094a1e96a307e00a
Teo Ljungberg <teo@teoljungberg.com>
2015-02-16 13:35:39 +0100

    Add fallback to timeout

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -24,4 +24,4 @@ Resque.after_fork do
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
 
-Resque::Plugins::Timeout.timeout = ENV["RESQUE_TIMEOUT"]
+Resque::Plugins::Timeout.timeout = (ENV["RESQUE_TIMEOUT"] || 60).to_i
